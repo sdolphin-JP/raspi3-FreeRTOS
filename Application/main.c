1758a9eb25c277ace4741ca4b6e8400950f87874
sdolphin <sdolphin@sdolphin.jp>
2021-12-05 13:57:52 +0900

    Add applications

---
+++ Application/main.c
@@ -0,0 +1,12 @@
+#include <stdint.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "syscalls.h"
+#include "hwaccess.h"
+
+void main(void)
+{
+    printf("Hello FreeRTOS \r\n");
+    while (1);
+}
